8ca1212096c2d646deba831f1ca38e77781b2275
Moin Shaikh <shaikhmoin250@gmail.com>
2018-01-05 14:48:11 +0530

    imported files body-parser

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
 const mongoose = require('mongoose');
 const config=require('./config/database');
 const path = require('path');
+const authentication = require('./routes/authentication')(router);
+const bodyParser = require('body-parser');
 
 mongoose.Promise=global.Promise;
 mongoose.connect(config.uri, (err)=>{
@@ -15,7 +18,15 @@ mongoose.connect(config.uri, (err)=>{
             }
 });
 
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 app.use(express.static(__dirname + '/client/dist/'));
+app.use('/authentication', authentication);
+
+
 app.get('*', (req, res)=> {
     res.sendFile(path.join(__dirname + '/client/dist/index.html'));
 });
