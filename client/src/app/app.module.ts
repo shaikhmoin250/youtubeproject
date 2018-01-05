bf197dd9d807289dcf87378e6803a52d357eec2c
Moin Shaikh <shaikhmoin250@gmail.com>
2018-01-05 14:52:25 +0530

    imported files

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule} from '@angular/http';
+import { AppRoutingModule} from './app.routing.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import {HomeComponent} from './components/home/home.component';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DashboardComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
