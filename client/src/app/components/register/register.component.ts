3e9e6245c88def6a7d692d4a333679b980fd2c4a
Moin Shaikh <shaikhmoin250@gmail.com>
2018-01-18 20:52:55 +0530

    changes applied

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -10,9 +12,18 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
 
   form:FormGroup;
+  message;
+  messageClass;
+  processing=false;
+  emailValid;
+  emailMessage;
+  usernameValid;
+  usernameMessage;
 
    constructor(
-    private formBuilder:FormBuilder
+    private formBuilder:FormBuilder,
+    private authService:AuthService,
+    private router:Router
   ) {
     this.createForm();
    }
@@ -44,6 +55,20 @@ export class RegisterComponent implements OnInit {
       confirm:['',Validators.required]
     },{validator:this.matchingPasswords('password' , 'confirm')})
   }
+
+  disableForm(){
+    this.form.controls['email'].disable();
+    this.form.controls['username'].disable();
+    this.form.controls['password'].disable();
+    this.form.controls['confirm'].disable();
+  }
+
+enableForm(){
+    this.form.controls['email'].enable();
+    this.form.controls['username'].enable();
+    this.form.controls['password'].enable();
+    this.form.controls['confirm'].enable();
+}
 
   validateEmail(controls){
     const regExp= new
@@ -90,8 +115,57 @@ matchingPasswords(password,confirm)
 }
 }
 onRegisterSubmit(){
-  console.log('form submitted');
+  this.processing=true;
+  this.disableForm();
+  const user={
+    email:this.form.get('email').value,
+    username:this.form.get('username').value,
+    password:this.form.get('password').value,
+    confirm:this.form.get('confirm').value
+  }
+
+  this.authService.registerUser(user).subscribe(data=>{
+    if(!data.success){
+      this.messageClass ='alert alert-danger';
+      this.message= data.message;
+      this.processing=false;
+      this.enableForm();
+    } else{
+      this.messageClass ='alert alert-success';
+      this.message= data.message;
+      setTimeout(()=> {
+        this.router.navigate(['/login']);
+      },2000)
+    }
+  });
+
 }
+
+checkEmail(){
+  this.authService.checkEmail(this.form.get('email').value).subscribe(data=>{
+    if(!data.success){
+      this.emailValid=false;
+      this.emailMessage= data.message;
+    }else{
+      this.emailValid= true;
+      this.emailMessage= data.message;
+    }
+  });
+}
+
+checkUsername(){
+  this.authService.checkUsername(this.form.get('username').value).subscribe(data=>{
+    if(!data.success){
+      this.usernameValid=false;
+      this.usernameMessage= data.message;
+    }else{
+      this.usernameValid= true;
+      this.usernameMessage= data.message;
+    }
+  });
+}
+
+
 
   ngOnInit() {
   }
